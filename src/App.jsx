e3d02a673c17b91eb4e4160a852042ffc80c2edc
mdallamaiqbal <bd999plus13@gmail.com>
2026-03-29 21:41:03 +0600

    add ai model hub design

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+   <Navbar/>
+   <Banner/>
+   <Footer/>
+    </>
+  )
+}
+
+export default App
